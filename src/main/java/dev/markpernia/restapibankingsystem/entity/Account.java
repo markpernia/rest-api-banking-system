a95f265719e6a7fd995be5e79a2fd39a9f72cb49
markpernia <perniamanila@gmail.com>
2023-12-24 12:14:36 +0100

    added entities with OneToOne relationship

---
+++ src/main/java/dev/markpernia/restapibankingsystem/entity/Account.java
@@ -0,0 +1,23 @@
+package dev.markpernia.restapibankingsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String accountType;
+    private Double balance;
+    private final LocalDate openedDate = LocalDate.now();
+
+    @OneToOne
+    private Customer customer;
+}
---
+++ src/main/java/dev/markpernia/restapibankingsystem/entity/Customer.java
@@ -0,0 +1,22 @@
+package dev.markpernia.restapibankingsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
+    @OneToOne(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Account account;
+}
