1c4b74b0b1a8477812415cf7e081eaaa86a1313b
markpernia <perniamanila@gmail.com>
2023-12-28 11:46:14 +0100

    added POST endpoint add customer

--- src/main/java/dev/markpernia/restapibankingsystem/controller/APIController.java
+++ src/main/java/dev/markpernia/restapibankingsystem/controller/APIController.java
@@ -5,10 +5,9 @@ import dev.markpernia.restapibankingsystem.dto.ErrorDTO;
 import dev.markpernia.restapibankingsystem.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("api/v1/customers")
@@ -24,11 +23,21 @@ public class APIController {
     @GetMapping("/{customerId}")
     public ResponseEntity<?> getCustomerDetails(@PathVariable Long customerId) throws Exception {
          try {
-             CustomerDTO customerDTO = customerService.findCustomerById(customerId);
-             return ResponseEntity.ok().body(customerDTO);
+             return ResponseEntity.ok().body(customerService.findCustomerById(customerId));
          } catch (Exception e) {
              return ResponseEntity.badRequest().body(new ErrorDTO(e.getMessage()));
          }
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> createCustomer(@RequestBody CustomerDTO customerDTO) {
+
+        try {
+            customerService.save(customerDTO);
+            return ResponseEntity.created(URI.create("/")).body(customerDTO);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ErrorDTO(e.getMessage()));
+        }
     }
 
 }
